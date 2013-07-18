94b59211a904be6f840f1605f343341cd658cdad
Kevin Miller <kemiller@csumb.edu>
2013-07-18 09:08:33 -0700

    Added yarn.

--- _posts/2013-07-15-my-first-post.markdown
+++
@@ -1,8 +0,0 @@
----
-layout: post
-title: "My First Post"
-tags:
- - blog
----
-
-This is my first post!  Don't I rule?
--- js/kevee.js
+++ js/kevee.js
@@ -2,5 +2,13 @@
 
 	$(document).ready(function() {
 		$('.row h2').sticky();
+		$('a.popover-link').popover({
+			html : true,
+			content : function() {
+				return $('#' + $(this).data('contentid')).find('div').first().html();
+			},
+			trigger : 'hover',
+			placement : 'bottom'
+		});
 	});
 })(jQuery);
