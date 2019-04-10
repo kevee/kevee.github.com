f39ff660e069b3e1c7838f3168c228520ce14ccb
Kevin Miller <kevin@csumb.edu>
2019-04-10 15:03:32 -0300

    Specifically import fonts

--- src/style/typography.js
+++ src/style/typography.js
@@ -1,5 +1,20 @@
 import Typography from 'typography'
 import './fonts.css'
+import '../assets/webfonts/PublicSans-Light.woff2'
+import '../assets/webfonts/PublicSans-Light.woff'
+import '../assets/webfonts/PublicSans-Light.ttf'
+
+import '../assets/webfonts/PublicSans-Black.woff2'
+import '../assets/webfonts/PublicSans-Black.woff'
+import '../assets/webfonts/PublicSans-Black.ttf'
+
+import '../assets/webfonts/PublicSans-Thin.woff2'
+import '../assets/webfonts/PublicSans-Thin.woff'
+import '../assets/webfonts/PublicSans-Thin.ttf'
+
+import '../assets/webfonts/PublicSans-Bold.woff2'
+import '../assets/webfonts/PublicSans-Bold.woff'
+import '../assets/webfonts/PublicSans-Bold.ttf'
 
 const backupFonts = [
   'Helvetica Neue',
