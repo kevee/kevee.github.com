a72fa1dc0e5c604ff364cf32c2bc68b451ecb98f
Kevin Miller <kevin@csumb.edu>
2019-04-18 10:53:08 -0300

    Everything is great now

--- src/components/frontpage/index.js
+++ src/components/frontpage/index.js
@@ -2,8 +2,9 @@ import React from 'react'
 import styled from '@emotion/styled'
 import bp from '../../style/breakpoints'
 import images from './images'
+import VisuallyHidden from '@reach/visually-hidden'
 
-const speed = 150
+const speed = 140
 
 const FlipbookContainer = styled('div')`
   ${bp({
@@ -31,35 +32,87 @@ const FlipbookName = styled('h1')`
   max-width: 400px;
 `
 
+const FlipbookRestartButton = styled('button')`
+  position: absolute;
+  bottom: 15px;
+  right: 15px;
+  background: transparent;
+  color: #fff;
+  text-decoration: underline;
+  border: 0;
+  z-index: 1000;
+  cursor: pointer;
+`
+
+const FlipbookCorrection = styled('span')`
+  color: #af2424;
+`
+
 class FrontpageHero extends React.Component {
   state = {
     currentImage: 0,
     count: 0,
+    animationComplete: false,
   }
 
   componentDidMount() {
+    this.startAnimation()
+  }
+
+  startAnimation() {
+    this.setState({
+      animationComplete: false,
+      currentImage: 0,
+    })
     this.interval = setInterval(() => {
       if (this.state.currentImage < images.length - 1) {
         this.setState(state => ({
           currentImage: state.currentImage + 1,
         }))
       } else {
-        this.setState(state => ({
-          currentImage: 0,
-        }))
+        this.stopAnimation()
       }
     }, speed)
   }
 
+  stopAnimation() {
+    this.setState(state => ({
+      animationComplete: true,
+    }))
+    clearInterval(this.interval)
+  }
+
   componentWillUnmount() {
-    //clearInterval(this.interval)
+    this.stopAnimation()
   }
 
   render() {
-    const { count, currentImage } = this.state
+    const { count, currentImage, animationComplete } = this.state
     return (
       <FlipbookContainer>
-        <FlipbookName>Hi, I'm Kevin Miller</FlipbookName>
+        <FlipbookName>
+          Hi,{' '}
+          {currentImage < 30 ? (
+            <>I'm</>
+          ) : (
+            <>
+              <strike>I'm</strike>{' '}
+              <FlipbookCorrection>I was</FlipbookCorrection>
+            </>
+          )}
+          <br />
+          Kevin Miller
+        </FlipbookName>
+        <VisuallyHidden>
+          This page includes a flipbook animation of me sitting in a bathtub,
+          minding my own business, when a tentacle appears and drags me under
+          the water.
+        </VisuallyHidden>
+        {animationComplete && (
+          <FlipbookRestartButton onClick={this.startAnimation.bind(this)}>
+            Replay
+          </FlipbookRestartButton>
+        )}
         {count < images.length && (
           <FlipbookImage src={images[0]} isActive={true} alt="" />
         )}
