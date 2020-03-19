07344a00a4531e32f1c2bd0da4acc9c6bdb6ecea
Kevin Miller <kevin@kevee.net>
2020-03-19 11:52:25 -0400

    What I've been up to

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,6 +24,11 @@ const HomeList = styled('ul')`
   font-size: 1.1rem;
 `
 
+const HomeStrong = styled('strong')`
+  font-family: Public Sans black;
+  margin-right: 0.5rem;
+`
+
 const IndexPage = ({ data }) => (
   <>
     <Helmet>
@@ -44,11 +49,39 @@ const IndexPage = ({ data }) => (
         and thoughtful design can have on people's lives.
       </HomeParagraph>
 
+      <h2>What I've been up to</h2>
+      <HomeParagraph>
+        In 2019, I was living abroad and had time to work on some passion
+        projects. These include:
+      </HomeParagraph>
+      <ul>
+        <li>
+          <HomeStrong>
+            <a href="https://neaps.js.org/">Neaps</a>
+          </HomeStrong>{' '}
+          Tidal height predictor for Javascript.
+        </li>
+        <li>
+          <HomeStrong>
+            <a href="https://github.com/gatsbyjs/gatsby/commits?author=kevee">
+              Gatsby documentation
+            </a>
+          </HomeStrong>{' '}
+          Helping the Gatsby team add and reorganize documentation.
+        </li>
+        <li>
+          <HomeStrong>
+            <a href="https://mocoloco.org">Mocoloco</a>
+          </HomeStrong>{' '}
+          A listing of all public agencies and services in Monterey County,
+          California.
+        </li>
+      </ul>
+
       <h2>Let's work together</h2>
       <HomeParagraph>
-        I am living in Argentina for 2019, and am available{' '}
-        <Link to="/contact">to work on your next project</Link>. I currently{' '}
-        love:
+        I am available <Link to="/contact">to work on your next project</Link>.
+        I currently love:
       </HomeParagraph>
       <HomeList>
         <li>
